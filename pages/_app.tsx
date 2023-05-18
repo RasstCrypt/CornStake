7178fbfd90f804ce4f059930e2712f909927aa90
RastCrypt <48489394+RasstCrypt@users.noreply.github.com>
2023-05-19 04:37:52 +0700

    Update _app.tsx
    base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { BaseGoerli } from "@thirdweb-dev/chains";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
+// const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={BaseGoerli}>
       <ChakraProvider>
         <Navbar />
         <Component {...pageProps} />
